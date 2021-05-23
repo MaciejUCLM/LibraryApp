bf5e8a549960190da77fa5f1efe010b9bc99c638
Piotr <piotr10tutek@poczta.onet.pl>
2021-05-23 22:17:38 +0200

    Basic shape of the app

--- Player.xaml.cs
+++ Player.xaml.cs
@@ -13,24 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace LibraryApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Player : Page
     {
-        public MainPage()
+        public Player()
         {
             this.InitializeComponent();
-            MainFrame.Navigate(typeof(Browse));
-        }
-
-        private void GoToDescription(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Navigate(typeof(Description));
         }
     }
 }
