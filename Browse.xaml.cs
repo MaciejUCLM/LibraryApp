bb8cfdb17b89207b7e5b18569be27636daad3729
Piotr <piotr10tutek@poczta.onet.pl>
2021-05-26 12:58:52 +0200

    Solving items hiding behind a gray item

--- Browse.xaml.cs
+++ Browse.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Reflection;
+using System.Xml;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,14 +21,12 @@ namespace LibraryApp
             this.InitializeComponent();
         }
 
-        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        public static T CloneXaml<T>(T source)
         {
-
+            EmptyPane pane = new EmptyPane();
+            return default;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
     }
 }
