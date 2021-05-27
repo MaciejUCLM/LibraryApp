38041a52d82e79d29662b32de1fb8d883990da60
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2021-05-27 19:15:16 +0200

    Added Persistence class

---
+++ Persistence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace LibraryApp
+{
+    class Persistence
+    {
+        private static Persistence instance = null;
+        public static Persistence GetInstance()
+        {
+            if (instance == null)
+                instance = new Persistence();
+            return instance;
+        }
+
+        public string ScanPath { get; set; }
+
+        private Persistence()
+        {
+        }
+
+        public bool ScanPathExists() => Directory.Exists(ScanPath);
+
+        public void Scan()
+        {
+            if (!ScanPathExists())
+                throw new DirectoryNotFoundException("The specified library location has not been found");
+            throw new NotImplementedException("Library scanning has not been implemented");
+        }
+    }
+}
