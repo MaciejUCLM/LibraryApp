ee4583daac6c7b6ba0a476c1e31be03d98ae3380
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2021-05-27 19:26:44 +0200

    Videos renamed

---
+++ Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    class Video
+    {
+        public string Title { get; set; }
+
+        public Video(string title, string metaname)
+        {
+            Title = title;
+        }
+    }
+}
